55b4a50f5fe12b3bdddf8d8ac8870fa21eb616ce
Cid <s.kaudelka@gmx.de>
2018-12-11 20:49:13 +0100

    Sudoku continued

--- WorkInProgress/Versionen - Sudoku/Sudoku 3.0/POSITION.java
+++ WorkInProgress/Versionen - Sudoku/Sudoku 3.0/POSITION.java
@@ -1,11 +1,14 @@
 public class POSITION
 {
-    private BILD img;
+    public BILD img;
+    private boolean marked;
+    private boolean fixed;
     private int number;
     private int pos_x;
     private int pos_y;
     
     public POSITION(int p_x, int p_y){
+        number = 0;
         pos_x = p_x;
         pos_y = p_y;
         img = new BILD("images\\n0.png");
@@ -14,6 +17,8 @@ public class POSITION
     }
     
     public void ChangeNumber(int num){
+        marked = !marked;
+        if(fixed) return;
         number = num;
         img = new BILD("images\\n"+ num +".png");
         img.SetzeX(pos_x*44+2);
@@ -22,7 +27,17 @@ public class POSITION
     
     public void FixedNumber(int num){
         number = num;
+        fixed = true;
         img = new BILD("images\\n"+ num +"f.png");
+        img.SetzeX(pos_x*44+2);
+        img.SetzeY(pos_y*44+2);
+    }
+    
+    public void Select(){
+        marked = !marked;
+        if(fixed) return;
+        if(marked)img = new BILD("images\\n"+ number +"m.png");
+        else      img = new BILD("images\\n"+ number +".png");
         img.SetzeX(pos_x*44+2);
         img.SetzeY(pos_y*44+2);
     }
--- WorkInProgress/Versionen - Sudoku/Sudoku 3.0/SPIEL.java
+++ WorkInProgress/Versionen - Sudoku/Sudoku 3.0/SPIEL.java
@@ -9,6 +9,10 @@ public class SPIEL
     //first [] == y, second [] == x
     private POSITION[][] pos;
     
+    private POSITION marked;
+    
+    private boolean NIsPressed;
+    
     public SPIEL()
     {
         background = new BILD("images\\background.png");
@@ -43,18 +47,83 @@ public class SPIEL
 
     public void TastenPruefen(KEYSTATE keystate)
     {
-        if(keystate.IsPressed("B")) //Bsp
-        {
-            
+        if(keystate.IsPressed("N") && NIsPressed){}else NIsPressed = false;
+        if(keystate.IsPressed("N") && !NIsPressed){
+            NIsPressed = true;
+            marked = null;
+            int[][] T = TEMPLATE.NewTemplate();
+            /*
+             * TODO: randomise Templates 
+             */
+            for(int i = 0; i < 9; i++)
+                for(int j = 0; j < 9; j++){
+                    pos[i][j] = new POSITION(i, j);
+                    if(T[i][j] != 0)pos[i][j].FixedNumber(T[i][j]);
+                }
+        }
+        if(marked != null){
+            if(keystate.IsPressed("0")){
+                marked.ChangeNumber(0);
+                marked = null;
+            }
+            if(keystate.IsPressed("1")){
+                marked.ChangeNumber(1);
+                marked = null;
+            }
+            if(keystate.IsPressed("2")){
+                marked.ChangeNumber(2);
+                marked = null;
+            }
+            if(keystate.IsPressed("3")){
+                marked.ChangeNumber(3);
+                marked = null;
+            }
+            if(keystate.IsPressed("4")){
+                marked.ChangeNumber(4);
+                marked = null;
+            }
+            if(keystate.IsPressed("5")){
+                marked.ChangeNumber(5);
+                marked = null;
+            }
+            if(keystate.IsPressed("6")){
+                marked.ChangeNumber(6);
+                marked = null;
+            }
+            if(keystate.IsPressed("7")){
+                marked.ChangeNumber(7);
+                marked = null;
+            }
+            if(keystate.IsPressed("8")){
+                marked.ChangeNumber(8);
+                marked = null;
+            }
+            if(keystate.IsPressed("9")){
+                marked.ChangeNumber(9);
+                marked = null;
+            }
         }
     }
 
     public void MausklickUeberpruefen(Point m)
     {
-        if(m.x >= 560 && m.x <= 601 && m.y>=759 && m.y<=800) //Bsp
-        {
-            
-        }
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++)
+                if(pos[j][i].img.PunktBeruehrt(m)){
+                    if(marked == null){//no pos marked
+                        pos[j][i].Select();
+                        marked = pos[j][i];
+                    } else {
+                        if(!pos[j][i].equals(marked)){//different pos marked
+                            marked.Select();
+                            pos[j][i].Select();
+                            marked = pos[j][i];
+                        } else {//same pos marked
+                            pos[j][i].Select();
+                            marked = null;
+                        }
+                    }
+                }
     }
 
     public void Ausgabe()
